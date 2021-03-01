c6672c9dee193dcb7485de416209b21c0c71b776
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2021-03-01 15:39:40 -0500

    GoogleAuthentication added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
     return (
@@ -7,9 +8,13 @@ const Header = () => {
             <Link to='/' className="item">Streamer</Link>
             <div className="right menu">
                 <Link to="/" className="item">All Streams</Link>
+                <GoogleAuth></GoogleAuth>
             </div>
         </div>
     );
 };
 
 export default Header;
+
+
+//key= '312689023447-i0mbqcesifrifp7bsoa5hjatfesfl3i6.apps.googleusercontent.com';
